fe6a873b72120cc26810cd38e991922b99262e46
jbigel <jacob.l.bigelow@gmail.com>
2024-07-30 14:50:41 -0400

    making pr comment a common action

--- cypress/support/cypress-support/src/config.js
+++ cypress/support/cypress-support/src/config.js
@@ -15,7 +15,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-const requestModules = 'requestModules';
+let requestModules = 'requestModules';
 const responseModules = 'responseModules';
 const defaultModule = 'defaultModule';
 const defaultMethod = 'defaultMethod';
